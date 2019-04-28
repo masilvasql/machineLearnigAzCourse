79559dee8ec4d8fb4c2b3d1ce6868598fed0cfb4
Marcelo Abrão da Silva <masilvasql@gmail.com>
2019-04-28 18:20:17 -0300

    Tratamento de valores faltantes - base crédito

--- Pre_processamento_com_pandas_e_scikit-learn/pre_process_credit_data.py
+++ Pre_processamento_com_pandas_e_scikit-learn/pre_process_credit_data.py
@@ -27,3 +27,20 @@ mediaDadosBase = base.mean() # comando traz a média
 mediaIdade = base['age'].mean() #apenas a média de idades
 mediaIdadeCorreta = base['age'][base.age > 0].mean()
 base.loc[base.age < 0 , 'age'] = 40.92
+
+valoresNulos = pd.isnull(base['age']) #recurso do python
+valloresNulosPandas = base.loc[pd.isnull(base['age'])]
+
+#dividindo a base entre atributos previsores e atributos de classe
+#iloc usado pra fazer a divisão da base
+# : (dois pontos) = PEGA todas as linhas
+# 1:4, separa a base do atributo 1 ao 4
+# do income até o loan, 1:4 (o último valor definido não entra na variável)
+previsores = base.iloc[:, 1:4].values
+classe = base.iloc[:, 4].values
+
+#tratando valores nulos
+# imputer vai auxiliar para preencher valores faltantes
+from sklearn.preprocessing import Imputer
+#ctrl + i = mostra informações da classe
+imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
